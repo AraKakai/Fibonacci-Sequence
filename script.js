6b864f0914828e886b0239aa65611bc613cd60e8
Ara Mikaela dela Cruz <delacruzkakai2@gmail.com>
2023-09-13 12:47:32 +0800

    Second Commit

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
-const generateFibonacci = n => {
-    const fibonacciSequence = [0, 1];
-    while (fibonacciSequence.length < n) {
-        fibonacciSequence.push(fibonacciSequence[fibonacciSequence.length - 1] + fibonacciSequence[fibonacciSequence.length - 2]);
+document.getElementById('generateButton').addEventListener('click', function () {
+    const numberOfTerms = prompt('Enter the number of Fibonacci terms to generate:');
+    if (numberOfTerms === null || isNaN(numberOfTerms)) {
+        alert('Please enter a valid number.');
+        return;
     }
+
+    const fibonacciSequence = generateFibonacci(Number(numberOfTerms));
+    document.getElementById('result').textContent = `Fibonacci Sequence: ${fibonacciSequence.join(', ')}`;
+});
+
+function generateFibonacci(n) {
+    const fibonacciSequence = [0, 1];
+
+    for (let i = 2; i < n; i++) {
+        const nextTerm = fibonacciSequence[i - 1] + fibonacciSequence[i - 2];
+        fibonacciSequence.push(nextTerm);
+    }
+
     return fibonacciSequence;
 }
-const numberOfTerms = 10;
-const fibonacciSeries = generateFibonacci(numberOfTerms);
-console.log(fibonacciSeries);
